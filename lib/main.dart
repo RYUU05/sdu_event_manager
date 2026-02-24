92b5344b14c5d90cefbc9b01438a58c7b55363ec
Nursultan Nazarbay <nursultan@MacBook-Air-Nursultan.local>
2026-02-24 21:01:06 +0500

    i added login and sign out page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:event_manager/firebase_options.dart';
-import 'package:event_manager/view/login_view.dart';
+import 'package:event_manager/screens/auth/login_screen.dart';
+import 'package:event_manager/screens/auth/register_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   WidgetsFlutterBinding.ensureInitialized();
+  await Future.delayed(const Duration(seconds: 2));
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
@@ -18,7 +24,11 @@ class MyApp extends StatelessWidget {
       title: 'Sdu Event Manager',
       theme: ThemeData(primarySwatch: Colors.blue),
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+      },
     );
   }
 }
