4b96468029fac0fcda9c74ceef676f02df5676b3
RYUU05 <109832735+RYUU05@users.noreply.github.com>
2026-04-15 04:36:59 +0500

    datasources added

--- lib/features/settings/data/datasources/settings_data_source.dart
+++ lib/features/settings/data/datasources/settings_data_source.dart
@@ -1,1 +1,13 @@
-class SettingsDataSource {}
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingsDataSource {
+  Future<void> logout() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+  }
+
+  Future<void> changeLanguage(String code) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('language', code);
+  }
+}
