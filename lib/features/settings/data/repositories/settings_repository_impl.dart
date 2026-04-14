fe576dadd86d2a3f8cbbfe3455f006428f9e25ff
RYUU05 <109832735+RYUU05@users.noreply.github.com>
2026-04-15 04:53:23 +0500

    repository implementation added

--- lib/features/settings/data/repositories/settings_repository_impl.dart
+++ lib/features/settings/data/repositories/settings_repository_impl.dart
@@ -1,1 +1,22 @@
-class SettingsRepositoryImpl {}
+import 'package:event_manager/features/settings/domain/entities/settings_entity.dart';
+import 'package:event_manager/features/settings/domain/repositories/settings_repository.dart';
+
+class SettingsRepositoryImpl implements SettingsRepository {
+  @override
+  Future<void> changeLang(String code) {
+    // TODO: implement changeLang
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<SettingsEntity> getSettings() {
+    // TODO: implement getSettings
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> logout() {
+    // TODO: implement logout
+    throw UnimplementedError();
+  }
+}
