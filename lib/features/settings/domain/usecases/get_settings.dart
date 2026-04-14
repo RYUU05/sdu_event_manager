e4186bdcd4be5f006f99d99a60464987e70962bf
RYUU05 <109832735+RYUU05@users.noreply.github.com>
2026-04-15 04:26:09 +0500

    domain layer updated

--- lib/features/settings/domain/usecases/get_settings.dart
+++ lib/features/settings/domain/usecases/get_settings.dart
@@ -1,1 +1,7 @@
-class GetSettings {}
+import 'package:event_manager/features/settings/domain/repositories/settings_repository.dart';
+
+class LogOutUseCases {
+  final SettingsRepository _repository;
+  LogOutUseCases(this._repository);
+  Future<void> call() => _repository.logout();
+}
