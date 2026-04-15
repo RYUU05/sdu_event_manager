a4cfeb2ba7de3e2077bb887cd2796a3600ad28b9
RYUU05 <109832735+RYUU05@users.noreply.github.com>
2026-04-15 23:51:45 +0500

    bloc for settings done

--- lib/features/settings/presentation/bloc/settings_bloc.dart
+++ lib/features/settings/presentation/bloc/settings_bloc.dart
@@ -1,7 +1,38 @@
+import 'package:event_manager/features/settings/domain/repositories/settings_repository.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 part 'settings_event.dart';
 part 'settings_state.dart';
 
 class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
-  SettingsBloc() : super(SettingsInitial());
+  final SettingsRepository repository;
+  SettingsBloc(this.repository) : super(SettingsInitial()) {
+    on<LoadSettingsEvent>(_onLoadSettings);
+    on<LanguageEvent>(_onChangeLanguage);
+    on<LogoutEvent>(_onLogout);
+  }
+
+  Future<void> _onLoadSettings(
+    LoadSettingsEvent event,
+    Emitter<SettingsState> emit,
+  ) async {
+    emit(SettingsLoading());
+    try {} catch (e) {
+      emit(SettingsError('Failed to load settings'));
+    }
+  }
+
+  Future<void> _onChangeLanguage(
+    LanguageEvent event,
+    Emitter<SettingsState> emit,
+  ) async {
+    try {} catch (e) {
+      emit(SettingsError('Failed to change lang'));
+    }
+  }
+}
+
+Future<void> _onLogout(LogoutEvent event, Emitter<SettingsState> emit) async {
+  try {} catch (e) {
+    emit(SettingsError('Failed to logout'));
+  }
 }
