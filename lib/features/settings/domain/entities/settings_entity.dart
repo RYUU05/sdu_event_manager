4e855d6b7d500ea06fd29634d42e36b2c92a41e6
RYUU05 <109832735+RYUU05@users.noreply.github.com>
2026-04-16 00:42:07 +0500

    settings_entity updated

--- lib/features/settings/domain/entities/settings_entity.dart
+++ lib/features/settings/domain/entities/settings_entity.dart
@@ -1,4 +1,22 @@
 class SettingsEntity {
   final String language;
+
   const SettingsEntity({required this.language});
+
+  SettingsEntity copyWith({String? language}) {
+    return SettingsEntity(language: language ?? this.language);
+  }
+
+  @override
+  String toString() => 'SettingsEntity(language: $language)';
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is SettingsEntity &&
+          runtimeType == other.runtimeType &&
+          language == other.language;
+
+  @override
+  int get hashCode => language.hashCode;
 }
