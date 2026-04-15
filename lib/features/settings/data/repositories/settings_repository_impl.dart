0a366f8a4a11d47f283396121e2b4dbe5b4a3d4e
RYUU05 <109832735+RYUU05@users.noreply.github.com>
2026-04-16 00:13:16 +0500

    setting_repo_impl added

--- lib/features/settings/data/repositories/settings_repository_impl.dart
+++ lib/features/settings/data/repositories/settings_repository_impl.dart
@@ -1,19 +1,25 @@
+import 'package:event_manager/features/settings/data/datasources/settings_data_source.dart';
 import 'package:event_manager/features/settings/domain/entities/settings_entity.dart';
 import 'package:event_manager/features/settings/domain/repositories/settings_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsRepositoryImpl implements SettingsRepository {
+  final SettingsDataSource dataSource;
+  SettingsRepositoryImpl(this.dataSource);
   @override
-  Future<void> changeLang(String code) {
-    throw UnimplementedError();
+  Future<void> changeLang(String code) async {
+    await dataSource.changeLanguage(code);
   }
 
   @override
-  Future<SettingsEntity> getSettings() {
-    throw UnimplementedError();
+  Future<SettingsEntity> getSettings() async {
+    final prefs = await SharedPreferences.getInstance();
+    final language = prefs.getString('language') ?? 'en';
+    return SettingsEntity(language: language);
   }
 
   @override
-  Future<void> logout() {
-    throw UnimplementedError();
+  Future<void> logout() async {
+    await dataSource.logout();
   }
 }
